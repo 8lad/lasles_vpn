5291cfb9e9a381b51bcbf88c69a9630ec6f2b70f
Vlad <vladisla1988@gmail.com>
2021-02-23 16:26:10 +0200

    added section subscribe and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
 $(function() {
 
+    $('.network__slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1500,
+        arrows: false,
+        dots: false
+    });
+
+    $('.customers__slider').slick({
+        infinite: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: true,
+        dots: true,
+        centerMode: true,
+        initialSlide: 1
+    });
+
 });
